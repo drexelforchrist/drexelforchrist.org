4fced672f8631658354eaefc0fae6794f7d10dc7
James Kurtz <jkrrv@jkrrv.org>
2015-06-15 17:41:25 -0400

    Added a PHP XSL parser for more useful mobile testing.

---
+++ page-church.php
@@ -0,0 +1,18 @@
+<?php
+
+// Load the XML source
+$xml = new DOMDocument;
+$xml->load('page-church.xml');
+
+$xsl = new DOMDocument;
+$xsl->load('template.xsl');
+
+// Configure the transformer
+$proc = new XSLTProcessor;
+$proc->importStyleSheet($xsl);
+
+//var_dump();
+
+echo $proc->transformToXML($xml);
+
+?>
