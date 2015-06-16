d85f294d55a41fc933c8d4ccf4be887aac1a241a
James Kurtz <jkrrv@jkrrv.org>
2015-06-15 21:49:00 -0400

    Bug Fixes & Improving Consistency

--- static/js/templating.js
+++ static/js/templating.js
@@ -11,8 +11,13 @@ document.addEventListener('scroll', function() {
             parseInt(document.defaultView.getComputedStyle(hs, '').getPropertyValue('margin-top')) +
             parseInt(document.defaultView.getComputedStyle(hs, '').getPropertyValue('margin-bottom')),
 
-        navH = '26';
+        navH = '26'; // KURTZ replace with a retrieved value.
 
+    if (scrollPos === undefined) { // cuz internet explorer.
+        scrollPos = window.pageYOffset;
+    }
+
+    //window.console.log("headH:", headH, "navH:", navH, "scrollPos", scrollPos);
     if (scrollPos >= (headH - navH) && !h.classList.contains('static')) {
         h.classList.add('static');
     } else if (scrollPos < (headH - navH) && h.classList.contains('static')) {
